0bc4007c9183cbbfdd838c2e0074d947a5d91ae0
dezska <104264903+antiglory@users.noreply.github.com>
2024-12-19 17:24:26 -0300

    ipdate main.c

--- source/main.c
+++ source/main.c
@@ -9,19 +9,22 @@
 jmp_buf env; // global var, we'll need this to handle errors
 int garbage_count = 0;
 
-void sigsegv_handler(int signum) {
+void sigsegv_handler(int signum)
+{
     printf("[!] signment error! signum: %d\n", signum);
     longjmp(env, 1);
 }
 
-int main(void) {
+int main(void)
+{
     signal(SIGSEGV, sigsegv_handler);
   
     void* start = malloc(RANGE);
     void* end = (char*)start + (RANGE);
     void* ptr = start;
 
-    while (ptr < end) {
+    while (ptr < end)
+    {
         if (setjmp(env) == 0)
             printf("[=] 00%-*p     : 0x%x\n", ptr, *(volatile char*)ptr); // prints the address and the respective value
         else
@@ -33,7 +36,8 @@ int main(void) {
     free(start);
     fflush(stdout);
   
-    if (garbage_count == 0) {
+    if (garbage_count == 0)
+    {
         printf("[!] nothing yet\n");
         printf("waiting client to close..\n");
 
